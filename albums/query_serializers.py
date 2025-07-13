0da9e703ceed79fc985761b81bc9a44db73314c4
shinminkyoung1 <dottiee0702@gmail.com>
2025-07-13 20:33:17 +0900

    feat: 대표사진 등록 파라미터 serializer 추가

--- albums/query_serializers.py
+++ albums/query_serializers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 
 class PlaceAlbumSerializer(serializers.Serializer):
     place = serializers.CharField(required = True)
+
+class FavoritePhotoSerializer(serializers.Serializer):
+    photo = serializers.CharField(required = True)
