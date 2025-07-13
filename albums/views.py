13a2041489f3ef1d7caa4404bd8cbe6712d57e08
shinminkyoung1 <dottiee0702@gmail.com>
2025-07-13 20:27:50 +0900

    feat: 앨범 목록 조회 기능 구현

---
+++ albums/views.py
@@ -0,0 +1,32 @@
+import logging
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import User, Photo, Album
+from utils.response_wrapper import api_response
+logger = logging.getLogger(__name__)
+
+# 앨범 목록 조회
+class AlbumListView(APIView):
+    def get(self, request):
+        try:
+            user = User.objects.get(userId=1)
+        except User.DoesNotExist:
+            return api_response(
+                status_code=status.HTTP_404_NOT_FOUND
+            )
+        
+        album_lists = Album.objects.filter(userId=user).select_related('placeId')
+
+        if not album_lists.exists():
+            return api_response(
+                is_success=True,
+                code='COMMON200',
+                message='앨범이 없습니다.',
+                status_code=status.HTTP_200_OK
+            )
+        
+        album_lists_name = [al.placeId.placeName for al in album_lists]
+
+        return api_response(
+            result=album_lists_name
+        )
