3b307224b848516f0aec23cee162c6192ca7b986
shinminkyoung1 <dottiee0702@gmail.com>
2025-07-17 01:06:31 +0900

    refactor: 장소이미지 필드 문자열 변경

--- places/models.py
+++ places/models.py
@@ -6,7 +6,7 @@ class Place(models.Model):
     placeId = models.BigAutoField(primary_key=True)
     placeName = models.CharField(max_length=255, db_column='placeName')
     area = models.CharField(max_length=255, null=True, blank=True)
-    placeImage = models.URLField(null=True, blank=True, db_column='placeImage')
+    placeImage = models.CharField(null=True, blank=True, db_column='placeImage')
     location = models.JSONField(default=list) # float의 List
 
     class Meta:
