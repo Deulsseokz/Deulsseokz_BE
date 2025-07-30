e980e141091163cb41863cd56e9ead1e323f0d6f
shinminkyoung1 <dottiee0702@gmail.com>
2025-07-30 22:58:34 +0900

    feat: 마이페이지 정보 수정

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ from .serializers import MypageInfoSerializer
 from utils.response_wrapper import api_response
 logger = logging.getLogger(__name__)
 
-# 마이페이지 정보 조회
 class MypageView(APIView):
+    # 마이페이지 정보 조회
     def get(self, request):
         try:
             user = User.objects.get(userId=1)
@@ -26,6 +26,28 @@ class MypageView(APIView):
         result.append(serializer.data)
 
         return api_response(result=result)
+    
+    # 마이페이지 정보 수정
+    def patch(self, request):
+        try:
+            user = User.objects.get(userId=1)
+        except User.DoesNotExist:
+            return api_response(
+                status_code=status.HTTP_404_NOT_FOUND
+            )
+        
+        # null 아닌 필드만 업데이트
+        update_fields = ['userName', 'profileImage']
+        for field in update_fields:
+            if field in request.data and request.data[field] is not None:
+                setattr(user, field, request.data[field])
+        user.save()
+
+        return api_response(
+            result="유저 정보가 성공적으로 수정되었습니다.",
+            status_code=status.HTTP_200_OK
+        )
+
 
 # 친구 목록 조회
 class FriendsListView(APIView):
