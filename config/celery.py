e1567dcd590e8c0a621b03aeb0480fdd1711750b
shinminkyoung1 <dottiee0702@gmail.com>
2025-09-06 13:47:16 +0900

    refactor: celery 호출 방식 직접 변경

--- config/celery.py
+++ config/celery.py
@@ -1,12 +1,10 @@
 import os
 from celery import Celery
 
-# Celery를 위해 기본 Django settings 모듈을 설정
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 app = Celery('config')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = 'amqp://guest:guest@rabbitmq:5672//'
 
-# 등록된 모든 Django 앱 설정에서 task 모듈을 자동으로 로드
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
