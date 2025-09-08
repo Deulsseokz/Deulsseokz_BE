2cc7c64af40ed05bb04912ca2f9b7ff2aa77d87e
shinminkyoung1 <dottiee0702@gmail.com>
2025-09-08 21:10:55 +0900

    fix: 주소 중복 지정 라인 제거

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
 
-app.conf.broker_url = 'amqp://guest:guest@rabbitmq:5672//'
+#app.conf.broker_url = 'amqp://guest:guest@rabbitmq:5672//'
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
