d8afae9f12fb531064894c72effc208d3e128409
shinminkyoung1 <dottiee0702@gmail.com>
2025-11-05 15:01:32 +0900

    fix: 애플로그인 fcm 토큰 발급 에러 해결

--- users/models.py
+++ users/models.py
@@ -20,8 +20,7 @@ class User(models.Model):
         on_delete=models.SET_NULL, # 뱃지가 삭제되어도 유저는 남도록 
         null=True
     )
-    fcm_token = models.CharField(max_length=255, null=True),
-    apple_sub = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    fcm_token = models.CharField(max_length=255, null=True)
 
     class Meta:
         db_table = 'User'
